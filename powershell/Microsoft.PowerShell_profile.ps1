b253637e648911a50c7683df20098de88ed00a1e
ChuyB <bovea3359@gmail.com>
2024-04-22 08:49:27 -0400

    updates config

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -39,3 +39,13 @@ New-Alias -Name "tile" -Value StartTiling
 New-Alias -Name "stoptile" -Value StopTiling
 New-Alias -Name "yasb" -Value StatusBar
 New-Alias -Name "ignite" -Value StartWorkspace
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
